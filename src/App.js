8bf9c2675cfb33251ca8ea3419f6d2f8a53f516b
adriansanz <alumno.50291@ies-azarquiel.es>
2024-01-26 18:05:14 +0100

    Prueba

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      
+      <div>
+        <div className="cabecera">
+          <div className="titulo">
+            <h1>Alonsopedia 14</h1>
+            <h3 className="subtitulo">
+              Tu Enciclopedia de Fernando Alonso de Confianza
+            </h3>
+          </div>
+          <div className="logo">
+            <img
+              className="imagenLogo"
+              src={logoAlonso}
+              alt="Descripción de la imagen"
+            />
+          </div>
+        </div>
+
+        <nav className="navegacion">
+          <Boton valor="Equipos" />
+          <Boton valor="Coches" />
+          <Boton
+            valor="Resultados"
+            componenteAEjecutar={ListaCarreras}
+            divId="contenido"
+          />
+        </nav>
+
+        <div id="contenido" className="contenido"></div>
+      </div>
     </div>
   );
 }
