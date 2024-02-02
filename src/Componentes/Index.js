4b3d5c560c1710ac83d30d7e6c977a237418f990
adriansanz <alumno.50291@ies-azarquiel.es>
2024-02-02 20:00:45 +0100

    Cambios menores en el css

--- src/Componentes/Index.js
+++ src/Componentes/Index.js
@@ -1,21 +1,15 @@
 import React from "react";
 import fotoAlonso from "../imagenes/alonso.jpg";
-import '../Estilos/Index.css';
+import "../Estilos/Index.css";
 
 function Index() {
   return (
-    <div className="contenedor">
-      <div className="imagen">
-        <img className="fotoAlonso" src={fotoAlonso} alt="Foto Alonso" />
+    <div class="contenedor">
+      <div class="div1">
       </div>
-      <div className="texto">
-        <p>
-          Fernando Alonso Díaz es un piloto de automovilismo nacido el 29 de
-          julio de 1981 en Oviedo, Asturias, España. Es reconocido como uno de
-          los mejores pilotos de Fórmula 1 de su generación. Alonso comenzó su
-          carrera en el karting a una edad temprana y rápidamente progresó a las
-          categorías de monoplazas.
-        </p>
+
+      <div class="div2">
+        <img className="fotoAlonso" src={fotoAlonso} alt="Imagen Alonso" />
       </div>
     </div>
   );
